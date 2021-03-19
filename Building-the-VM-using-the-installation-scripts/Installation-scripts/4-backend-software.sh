3885defc60e5bb3a230473236d1f9d1ce995c93a
Pieterl <pieterlems.personal@protonmail.com>
2021-03-19 23:40:42 +0100

    Update 4-backend-software.sh
    HotFix for PGAdmin install break  caused by python 3.8

--- Building-the-VM-using-the-installation-scripts/Installation-scripts/4-backend-software.sh
+++ Building-the-VM-using-the-installation-scripts/Installation-scripts/4-backend-software.sh
@@ -42,6 +42,14 @@ echo "-------------->>>> Changed <<<<--------------"
 
 echo "-------------->>>> Installing PGMyAdmin <<<<--------------"
 sleep 2
+
+# Install the public key for the repository (if not done previously):
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+
+
+# Create the repository configuration file:
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+
 # Install PGMyAdmin4 depending on the Ubuntu version
 sudo apt install pgadmin4
 
